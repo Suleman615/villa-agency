15cfc5f2297191f48d8c5d8f0249e2bcbf5b999c
Ghulam Suleman <sulemanbuzdar615@gmail.com>
2023-09-06 14:34:56 +0500

    properties detail page added

--- src/Components/SingleProperty.jsx
+++ src/Components/SingleProperty.jsx
@@ -18,7 +18,7 @@ const SingleProperty=()=>{
                     <p className='w-fit bg-orange-600 bg-opacity-30 px-3 py-1 rounded'>villa</p>
                     <h1 className='font-bold text-3xl my-6'>24 New Street Miami, OR 24560</h1>
                 </div>
-                <p className='leading-loose text-gray-500 '>Get the best villa agency HTML CSS Bootstrap Template for your company website. TemplateMo provides you the best free CSS templates in the world. Please tell your friends about it. Thank you. Cloud bread kogi bitters pitchfork shoreditch tumblr yr succulents single-origin coffee schlitz enamel pin you probably haven't heard of them ugh hella.</p>
+                <p className='leading-loose text-gray-500 '>Get <span className='font-bold'>the best villa agency</span> HTML CSS Bootstrap Template for your company website. TemplateMo provides you the <a className='text-blue-600 font-semibold ' href=''>best free CSS templates</a> in the world. Please tell your friends about it. Thank you. Cloud bread kogi bitters pitchfork shoreditch tumblr yr succulents single-origin coffee schlitz enamel pin you probably haven't heard of them ugh hella.</p>
                 <p className='leading-loose text-gray-500 py-6'>When you look for free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Digital Marketing, TemplateMo Corporate Layouts, etc. Master cleanse +1 intelligentsia swag post-ironic, slow-carb chambray knausgaard PBR&B DSA poutine neutra cardigan hoodie pop-up.</p>
 
                 {/**********Questions***************/}
